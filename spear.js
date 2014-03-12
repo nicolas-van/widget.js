00d951177d803c4ccfffc6d2063e8c846c287cb4
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2014-03-12 11:41:02 +0100

    Initial import

---
+++ spear.js
@@ -0,0 +1,457 @@
+/*
+Copyright (c) 2012, Nicolas Vanhoren
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+(function() {
+"use strict";
+
+if (typeof(define) !== "undefined") { // requirejs
+    define(["jquery", "underscore", "ring"], function() {
+        declare.call(null, [window.document].concat(arguments));
+    });
+} else if (typeof(exports) !== "undefined") { // node
+    module.exports = function(w) {
+        if (! w.document) {
+            throw new Error( "Spear.js requires a window with a document" );
+        }
+        var und = require("underscore");
+        var jq = require("jquery")(w);
+        var rg = require("ring");
+        return declare(w.document, jq, und, rg);
+    };
+} else { // define global variable 'spear'
+    window.spear = declare(window.document, $, _, ring);
+}
+
+function declare(document, $, _, ring) {
+    var spear = {};
+    spear.internal = {};
+
+    spear.$ = $;
+
+    /**
+     * Mixin to express the concept of destroying an object.
+     * When an object is destroyed, it should release any resource
+     * it could have reserved before.
+     */
+    spear.Destroyable = ring.create({
+        constructor: function() {
+            this.__destroyableDestroyed = false;
+        },
+        /**
+         * Returns true if destroy() was called on the current object.
+         */
+        isDestroyed : function() {
+            return this.__destroyableDestroyed;
+        },
+        /**
+         * Inform the object it should destroy itself, releasing any
+         * resource it could have reserved.
+         */
+        destroy : function() {
+            this.__destroyableDestroyed = true;
+        }
+    });
+
+    /**
+     * Mixin to structure objects' life-cycles folowing a parent-children
+     * relationship. Each object can a have a parent and multiple children.
+     * When an object is destroyed, all its children are destroyed too.
+     */
+    spear.Parented = ring.create([spear.Destroyable], {
+        __parentedMixin : true,
+        constructor: function(parent) {
+            this.$super();
+            this.__parentedChildren = [];
+            this.__parentedParent = null;
+            this.setParent(parent);
+        },
+        /**
+         * Set the parent of the current object. When calling this method, the
+         * parent will also be informed and will return the current object
+         * when its getChildren() method is called. If the current object did
+         * already have a parent, it is unregistered before, which means the
+         * previous parent will not return the current object anymore when its
+         * getChildren() method is called.
+         */
+        setParent : function(parent) {
+            if (this.getParent()) {
+                if (this.getParent().__parentedMixin) {
+                    this.getParent().__parentedChildren = _.without(this
+                            .getParent().getChildren(), this);
+                }
+            }
+            this.__parentedParent = parent;
+            if (parent && parent.__parentedMixin) {
+                parent.__parentedChildren.push(this);
+            }
+        },
+        /**
+         * Return the current parent of the object (or null).
+         */
+        getParent : function() {
+            return this.__parentedParent;
+        },
+        /**
+         * Return a list of the children of the current object.
+         */
+        getChildren : function() {
+            return _.clone(this.__parentedChildren);
+        },
+        destroy : function() {
+            _.each(this.getChildren(), function(el) {
+                el.destroy();
+            });
+            this.setParent(undefined);
+            this.$super();
+        }
+    });
+
+    /*
+     * Yes, we steal Backbone's events :)
+     * 
+     * This class just handle the dispatching of events, it is not meant to be extended,
+     * nor used directly. All integration with parenting and automatic unregistration of
+     * events is done in the EventDispatcher class.
+     */
+    // (c) 2010-2012 Jeremy Ashkenas, DocumentCloud Inc.
+    // Backbone may be freely distributed under the MIT license.
+    // For all details and documentation:
+    // http://backbonejs.org
+    spear.internal.Events = ring.create({
+        on : function(events, callback, context) {
+            var ev;
+            events = events.split(/\s+/);
+            var calls = this._callbacks || (this._callbacks = {});
+            while ((ev = events.shift())) {
+                var list = calls[ev] || (calls[ev] = {});
+                var tail = list.tail || (list.tail = list.next = {});
+                tail.callback = callback;
+                tail.context = context;
+                list.tail = tail.next = {};
+            }
+            return this;
+        },
+        off : function(events, callback, context) {
+            var ev, calls, node;
+            if (!events) {
+                delete this._callbacks;
+            } else if ((calls = this._callbacks)) {
+                events = events.split(/\s+/);
+                while ((ev = events.shift())) {
+                    node = calls[ev];
+                    delete calls[ev];
+                    if (!callback || !node)
+                        continue;
+                    while ((node = node.next) && node.next) {
+                        if (node.callback === callback &&
+                                (!context || node.context === context))
+                            continue;
+                        this.on(ev, node.callback, node.context);
+                    }
+                }
+            }
+            return this;
+        },
+        callbackList: function() {
+            var lst = [];
+            _.each(this._callbacks || {}, function(el, eventName) {
+                var node = el;
+                while ((node = node.next) && node.next) {
+                    lst.push([eventName, node.callback, node.context]);
+                }
+            });
+            return lst;
+        },
+        trigger : function(events) {
+            var event, node, calls, tail, args, all, rest;
+            if (!(calls = this._callbacks))
+                return this;
+            all = calls.all;
+            (events = events.split(/\s+/)).push(null);
+            // Save references to the current heads & tails.
+            while ((event = events.shift())) {
+                if (all)
+                    events.push({
+                        next : all.next,
+                        tail : all.tail,
+                        event : event
+                    });
+                if (!(node = calls[event]))
+                    continue;
+                events.push({
+                    next : node.next,
+                    tail : node.tail
+                });
+            }
+            rest = Array.prototype.slice.call(arguments, 1);
+            while ((node = events.pop())) {
+                tail = node.tail;
+                args = node.event ? [ node.event ].concat(rest) : rest;
+                while ((node = node.next) !== tail) {
+                    node.callback.apply(node.context || this, args);
+                }
+            }
+            return this;
+        }
+    });
+    // end of Backbone's events class
+    
+    spear.EventDispatcher = ring.create([spear.Parented], {
+        __eventDispatcherMixin: true,
+        constructor: function(parent) {
+            this.$super(parent);
+            this.__edispatcherEvents = new spear.internal.Events();
+            this.__edispatcherRegisteredEvents = [];
+        },
+        on: function(events, dest, func) {
+            var self = this;
+            events = events.split(/\s+/);
+            _.each(events, function(eventName) {
+                self.__edispatcherEvents.on(eventName, func, dest);
+                if (dest && dest.__eventDispatcherMixin) {
+                    dest.__edispatcherRegisteredEvents.push({name: eventName, func: func, source: self});
+                }
+            });
+            return this;
+        },
+        off: function(events, dest, func) {
+            var self = this;
+            events = events.split(/\s+/);
+            _.each(events, function(eventName) {
+                self.__edispatcherEvents.off(eventName, func, dest);
+                if (dest && dest.__eventDispatcherMixin) {
+                    dest.__edispatcherRegisteredEvents = _.filter(dest.__edispatcherRegisteredEvents, function(el) {
+                        return !(el.name === eventName && el.func === func && el.source === self);
+                    });
+                }
+            });
+            return this;
+        },
+        trigger: function(events) {
+            this.__edispatcherEvents.trigger.apply(this.__edispatcherEvents, arguments);
+            return this;
+        },
+        destroy: function() {
+            var self = this;
+            _.each(this.__edispatcherRegisteredEvents, function(event) {
+                event.source.__edispatcherEvents.off(event.name, event.func, self);
+            });
+            this.__edispatcherRegisteredEvents = [];
+            _.each(this.__edispatcherEvents.callbackList(), function(cal) {
+                this.off(cal[0], cal[2], cal[1]);
+            }, this);
+            this.__edispatcherEvents.off();
+            this.$super();
+        }
+    });
+    
+    spear.Properties = ring.create([spear.EventDispatcher], {
+        classInit: function(proto) {
+            var flat = _.extend({}, proto);
+            var props = {};
+            _.each(flat, function(v, k) {
+                if (typeof v === "function") {
+                    var res = /^((?:get)|(?:set))([A-Z]\w*)$/.exec(k);
+                    if (! res)
+                        return;
+                    var name = res[2][0].toLowerCase() + res[2].slice(1);
+                    var prop = props[name] || (props[name] = {});
+                    prop[res[1]] = v;
+                }
+            });
+            proto.__properties = props;
+        },
+        constructor: function(parent) {
+            this.$super(parent);
+            this.__dynamicProperties = {};
+        },
+        set: function(arg1, arg2) {
+            var self = this;
+            var map;
+            if (typeof arg1 === "string") {
+                map = {};
+                map[arg1] = arg2;
+            } else {
+                map = arg1;
+            }
+            var tmpSet = this.__propsSetting;
+            this.__propsSetting = false;
+            _.each(map, function(val, key) {
+                var prop = self.__properties[key];
+                if (prop) {
+                    if (! prop.set)
+                        throw new ring.InvalidArgumentError("Property " + key + " does not have a setter method.");
+                    prop.set.call(self, val);
+                } else {
+                    self.fallbackSet(key, val);
+                }
+            });
+            this.__propsSetting = tmpSet;
+            if (! this.__propsSetting && this.__propsSetted) {
+                this.__propsSetted = false;
+                self.trigger("change", self);
+            }
+        },
+        get: function(key) {
+            var prop = this.__properties[key];
+            if (prop) {
+                if (! prop.get)
+                    throw new ring.InvalidArgumentError("Property " + key + " does not have a getter method.");
+                return prop.get.call(this);
+            } else {
+                return this.fallbackGet(key);
+            }
+        },
+        fallbackSet: function(key, val) {
+            throw new ring.InvalidArgumentError("Property " + key + " is not defined.");
+        },
+        fallbackGet: function(key) {
+            throw new ring.InvalidArgumentError("Property " + key + " is not defined.");
+        },
+        trigger: function(name) {
+            this.$super.apply(this, arguments);
+            if (/(\s|^)change\:.*/.exec(name)) {
+                if (! this.__propsSetting)
+                    this.trigger("change");
+                else
+                    this.__propsSetted = true;
+            }
+        }
+    });
+
+    spear.DynamicProperties = ring.create([spear.Properties], {
+        constructor: function(parent) {
+            this.$super(parent);
+            this.__dynamicProperties = {};
+        },
+        fallbackSet: function(key, val) {
+            var tmp = this.__dynamicProperties[key];
+            if (tmp === val)
+                return;
+            this.__dynamicProperties[key] = val;
+            this.trigger("change:" + key, this, {
+                oldValue: tmp,
+                newValue: val
+            });
+        },
+        fallbackGet: function(key) {
+            return this.__dynamicProperties[key];
+        }
+    });
+    
+    spear.Widget = ring.create([spear.DynamicProperties], {
+        tagName: 'div',
+        className: '',
+        attributes: {},
+        events: {},
+        constructor: function(parent) {
+            this.$super(parent);
+            this.__widgetElement = $("<" + this.tagName + ">");
+            this.$().addClass(this.className);
+            _.each(this.attributes, function(val, key) {
+                this.$().attr(key, val);
+            }, this);
+            _.each(this.events, function(val, key) {
+                key = key.split(" ");
+                val = _.bind(typeof val === "string" ? this[val] : val, this);
+                if (key.length > 1) {
+                    this.$().on(key[0], key[1], val);
+                } else {
+                    this.$().on(key[0], val);
+                }
+            }, this);
+    
+            this.setParent(parent);
+        },
+        $: function(attr) {
+            if (attr)
+                return this.__widgetElement.find.apply(this.__widgetElement, arguments);
+            else
+                return this.__widgetElement;
+        },
+        /**
+         * Destroys the current widget, also destroys all its children before destroying itself.
+         */
+        destroy: function() {
+            _.each(this.getChildren(), function(el) {
+                el.destroy();
+            });
+            this.$().remove();
+            this.$super();
+        },
+        /**
+         * Renders the current widget and appends it to the given jQuery object or Widget.
+         *
+         * @param target A jQuery object or a Widget instance.
+         */
+        appendTo: function(target) {
+            this.$().appendTo(target);
+            return this.render();
+        },
+        /**
+         * Renders the current widget and prepends it to the given jQuery object or Widget.
+         *
+         * @param target A jQuery object or a Widget instance.
+         */
+        prependTo: function(target) {
+            this.$().prependTo(target);
+            return this.render();
+        },
+        /**
+         * Renders the current widget and inserts it after to the given jQuery object or Widget.
+         *
+         * @param target A jQuery object or a Widget instance.
+         */
+        insertAfter: function(target) {
+            this.$().insertAfter(target);
+            return this.render();
+        },
+        /**
+         * Renders the current widget and inserts it before to the given jQuery object or Widget.
+         *
+         * @param target A jQuery object or a Widget instance.
+         */
+        insertBefore: function(target) {
+            this.$().insertBefore(target);
+            return this.render();
+        },
+        /**
+         * Renders the current widget and replaces the given jQuery object.
+         *
+         * @param target A jQuery object or a Widget instance.
+         */
+        replace: function(target) {
+            this.$().replace(target);
+            return this.render();
+        },
+        /**
+         * This is the method to implement to render the Widget.
+         */
+        render: function() {}
+    });
+
+    return spear;
+}
+})();
---
+++ test.js
@@ -0,0 +1,237 @@
+
+(function() {
+"use strict";
+
+if (typeof(module) !== "undefined") {
+    global.ring = require("ring");
+    global.assert = require("assert");
+    global._ = require("underscore");
+    var jsdom = require("jsdom").jsdom;
+    var doc = jsdom();
+    var window = doc.parentWindow;
+    global.$ = require("jquery")(window);
+    global.spear = require("./spear")(window);
+}
+
+suite("Destroyable");
+
+test("base", function() {
+    var x = new spear.Destroyable();
+    assert.equal(!!x.isDestroyed(), false);
+    x.destroy();
+    assert.equal(x.isDestroyed(), true);
+});
+
+suite("Parented");
+
+test("base", function() {
+    var x = new spear.Parented();
+    var y = new spear.Parented();
+    y.setParent(x);
+    assert.equal(y.getParent(), x);
+    assert.equal(x.getChildren()[0], y);
+    x.destroy();
+    assert.equal(y.isDestroyed(), true);
+});
+
+suite("Events");
+
+test("base", function() {
+    var x = new spear.internal.Events();
+    var tmp = 0;
+    var fct = function() {tmp = 1;};
+    x.on("test", fct);
+    assert.equal(tmp, 0);
+    x.trigger("test");
+    assert.equal(tmp, 1);
+    tmp = 0;
+    x.off("test", fct);
+    x.trigger("test");
+    assert.equal(tmp, 0);
+});
+
+suite("EventDispatcher");
+
+test("base", function() {
+    var x = new spear.EventDispatcher();
+    var y = new spear.EventDispatcher();
+    var tmp = 0;
+    var fct = function() {tmp = 1;};
+    x.on("test", y, fct);
+    assert.equal(tmp, 0);
+    x.trigger("test");
+    assert.equal(tmp, 1);
+    tmp = 0;
+    x.off("test", y, fct);
+    x.trigger("test");
+    assert.equal(tmp, 0);
+    tmp = 0;
+    x.on("test", y, fct);
+    y.destroy();
+    x.trigger("test");
+    assert.equal(tmp, 0);
+});
+test("memoryLeak", function() {
+    var x = new spear.EventDispatcher();
+    var y = new spear.EventDispatcher();
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 0);
+    var fct = function() {};
+    x.on("test", y, fct);
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 1);
+    y.destroy();
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 0);
+
+    x = new spear.EventDispatcher();
+    y = new spear.EventDispatcher();
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 0);
+    fct = function() {};
+    x.on("test", y, fct);
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 1);
+    x.destroy();
+    assert.equal(x.__edispatcherRegisteredEvents.length, 0);
+    assert.equal(y.__edispatcherRegisteredEvents.length, 0);
+});
+
+suite("Properties");
+
+test("base", function() {
+    var Claz = ring.create([spear.Properties], {
+        getStuff: function() {
+            return this.stuff;
+        },
+        setStuff: function(val) {
+            this.stuff = val;
+        }
+    });
+    var Claz2 = ring.create([Claz], {
+        getThing: function() {
+            return this.thing;
+        },
+        setThing: function(val) {
+            this.thing = val;
+        }
+    });
+    var x = new Claz();
+    x.set("stuff", "stuff");
+    assert.equal(x.stuff, "stuff");
+    assert.equal(x.getStuff(), "stuff");
+    assert.equal(x.get("stuff"), "stuff");
+    x = new Claz();
+    x.setStuff("stuff");
+    assert.equal(x.stuff, "stuff");
+    assert.equal(x.getStuff(), "stuff");
+    assert.equal(x.get("stuff"), "stuff");
+    x = new Claz2();
+    x.set("stuff", "stuff");
+    assert.equal(x.stuff, "stuff");
+    assert.equal(x.getStuff(), "stuff");
+    assert.equal(x.get("stuff"), "stuff");
+    x.set("thing", "thing");
+    assert.equal(x.thing, "thing");
+    assert.equal(x.getThing(), "thing");
+    assert.equal(x.get("thing"), "thing");
+    x = new Claz2();
+    x.setStuff("stuff");
+    assert.equal(x.stuff, "stuff");
+    assert.equal(x.getStuff(), "stuff");
+    assert.equal(x.get("stuff"), "stuff");
+    x.setThing("thing");
+    assert.equal(x.thing, "thing");
+    assert.equal(x.getThing(), "thing");
+    assert.equal(x.get("thing"), "thing");
+});
+
+suite("DynamicProperties");
+
+test("base", function() {
+    var x = new spear.DynamicProperties();
+    var y = new spear.DynamicProperties();
+    x.set({test: 1});
+    assert.equal(x.get("test"), 1);
+    var tmp = 0;
+    x.on("change:test", y, function(model, options) {
+        tmp = 1;
+        assert.equal(options.oldValue, 1);
+        assert.equal(options.newValue, 2);
+        assert.equal(x.get("test"), 2);
+        assert.equal(model, x);
+    });
+    x.set({test: 2});
+    assert.equal(tmp, 1);
+});
+
+test("change event only when changed", function() {
+    var x = new spear.DynamicProperties();
+    var exec1 = false;
+    var exec2 = false;
+    x.on("change:test", null, function() {exec1 = true;});
+    x.on("change", null, function() {exec2 = true;});
+    x.set({"test": 3});
+    assert.equal(exec1, true);
+    assert.equal(exec2, true);
+    exec1 = false;
+    exec2 = false;
+    x.set({"test": 3});
+    assert.equal(exec1, false);
+    assert.equal(exec2, false);
+});
+
+suite("Widget");
+
+test("base", function() {
+    var Claz = ring.create([spear.Widget], {
+        tagName: "span",
+        className: "mytestspan",
+        attributes: {
+            "id": "testspan"
+        },
+        render: function() {
+            this.$().html("test");
+        }
+    });
+    var x = new Claz();
+    x.appendTo($("body"));
+    var $el = $("#testspan");
+    assert.equal($el.length, 1);
+    assert.equal($el.parents()[0], $("body")[0]);
+    assert.equal($el.html(), "test");
+    assert.equal($el[0], $("span.mytestspan")[0]);
+    
+    var y = new Claz(x);
+    assert.equal(y.getParent(), x);
+    
+    x.destroy();
+    $el = $("#testspan");
+    assert.equal($el.length, 0);
+});
+
+test("events", function() {
+    var test = 0;
+    var Claz = ring.create([spear.Widget], {
+        events: {
+            "testevent": function() {
+                test = 1;
+            },
+            "testevent2 .testspan": function() {
+                test = 2;
+            }
+        },
+        render: function() {
+            this.$().html("<span class='testspan'></span>");
+        }
+    });
+    var x = new Claz();
+    x.appendTo($("body"));
+    assert.equal(test, 0);
+    x.$().trigger("testevent");
+    assert.equal(test, 1);
+    x.$(".testspan").trigger("testevent2");
+    assert.equal(test, 2);
+});
+
+})();
